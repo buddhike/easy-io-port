0801a523a61996968bbbfc8db854ea9969553d59
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2016-06-02 17:20:52 +1000

    Initial implementation.

---
+++ test/index.js
@@ -0,0 +1,103 @@
+import sinon from 'sinon';
+import Q from 'q';
+
+import ioPort from '../lib/index';
+
+describe('io port', function () {
+  let port, listener;
+
+  beforeEach(() => {
+    port = ioPort();
+    listener = sinon.spy();
+  });
+
+  describe('input', () => {
+    it('should be delivered to listeners', () => {
+      const inMsg = {type: 'a', body: 'hello'};
+      const subscription = port.receive('a', listener);
+      port.input(JSON.stringify(inMsg));
+      listener.calledWithMatch(inMsg);
+      subscription.unsubscribe();
+    });
+
+    it('should ignore when there are no listeners', () => {
+      const input = () => port.input(JSON.stringify({type: 'a', body: 'foo'}));
+      input.should.not.throw();
+    });
+  });
+
+  describe('listener', () => {
+    let inMsg, subscription;
+
+    beforeEach(() => {
+      inMsg = {type: 'a', body: 'hello'};
+      subscription = port.receive('a', listener);
+    });
+
+    it('should only receive the interested messages', () => {
+      const inMsg2 = {type: 'b', body: 'world'};
+
+      port.input(JSON.stringify(inMsg));
+      port.input(JSON.stringify(inMsg2));
+
+      listener.neverCalledWithMatch(inMsg2).should.be.true;
+    });
+
+    it('should not receive messages after unsubscribe', () => {
+      subscription.unsubscribe();
+      port.input(JSON.stringify(inMsg));
+      listener.neverCalledWithMatch(inMsg).should.be.true;
+    });
+  });
+
+  describe('output', () => {
+    describe('sync void output', () => {
+      it('should work', async () => {
+        let output = sinon.spy();
+        port.bindOutput(output);
+
+        await port.send('a', 'foo');
+
+        output
+        .calledWith(JSON.stringify({type: 'a', body: 'foo'}))
+        .should
+        .be
+        .true;
+      });
+    });
+
+    describe('sync output', () => {
+      it('should work', async () => {
+        let output = sinon.stub().returns(1);
+        port.bindOutput(output);
+
+        await port.send('a', 'foo');
+
+        output
+        .calledWith(JSON.stringify({type: 'a', body: 'foo'}))
+        .should
+        .be
+        .true;
+      });
+    });
+
+    describe('async output', () => {
+      it('should await until output is done', async () => {
+        const deferred = Q.defer();
+        const output = sinon.stub().returns(deferred.promise);
+        let completed = false;
+
+        port.bindOutput(output);
+
+        const r = port.send('a', 'foo').then(() => completed = true);
+
+        completed.should.be.false;
+
+        deferred.resolve();
+        await r;
+        
+        completed.should.be.true;
+      });
+    });
+  });
+});
